6c1d4c71aeb9be9251b0a1357ebe7af10d6b5122
watchearnad <andiksugi980@gmail.com>
2025-09-07 16:52:57 +0700

    Update _db.js

--- api/_db.js
+++ api/_db.js
@@ -1,6 +1,15 @@
+// api/_db.js
 const { Pool } = require("pg");
+
 let pool;
-module.exports = () => (pool ??= new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-}));
+module.exports = function getPool() {
+  if (!pool) {
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL,   // pakai URI Supabase Pooler 6543 + ?sslmode=require
+      ssl: { rejectUnauthorized: false },
+      max: 5,
+      idleTimeoutMillis: 10000,
+    });
+  }
+  return pool;
+};
